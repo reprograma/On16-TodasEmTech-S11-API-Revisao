593e3c42e4a7f7726121f464d8ba95d86c2f0bb3
Lia Laurindo <liamaralaurindo@gmail.com>
2022-06-04 14:49:12 -0300

    configurando porta

--- para-sala/pet-shop-reprograma/server.js
+++ para-sala/pet-shop-reprograma/server.js
@@ -2,5 +2,5 @@ const app = require ('./src/app')
 const PORT = 1313;
 
 app.listen (PORT, () => {
-    console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
