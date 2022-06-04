95e6d93fee45c72c6188c0e96796ffba457b5c5a
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-06-04 13:43:56 -0300

    Criando nossas rotas

--- para-sala/pet-shop-reprograma/src/router/petsRoutes.js
+++ para-sala/pet-shop-reprograma/src/router/petsRoutes.js
@@ -1,11 +1,12 @@
-// Nossas rotas
+// Criando nossas rotas
 
-const express = require('express')
+const express = require('express') 
 
 const router = express.Router()
 
 const controller = require('../controller/petsController')
 
+// Rotas:
 router.post('/adicionar', controller.postPet)
 
 
