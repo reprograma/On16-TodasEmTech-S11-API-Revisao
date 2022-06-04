06145e6b7d95f41334608e116f05c8a4f2c49195
Lia Laurindo <liamaralaurindo@gmail.com>
2022-06-04 16:36:09 -0300

    inclusão de rotas POST e PATCH

--- para-sala/pet-shop-reprograma/src/controller/petsController.js
+++ para-sala/pet-shop-reprograma/src/controller/petsController.js
@@ -5,7 +5,6 @@ const pets = require('../models/pets.json')
 // importando o pacote fs > realizar alterações direto no pets.json
 const fs = require('fs')
 
-
 // localhost:1313/pets/adicionar
 const postPets = (req, res) => {
     //passando quais informações serão requeridas ao utilizar a rota post
@@ -27,22 +26,65 @@ const postPets = (req, res) => {
     })
 
     //usando o fs para ler o arquivo pets.json e incluir o novo elemento q vem da rota post
-    fs.writeFile ('.src/models/pets.json', JSON.stringfy(pets), 'utf8', function (err) {
+    fs.writeFile('.src/models/pets.json', JSON.stringfy(pets), 'utf8', function (err) {
+        if (err) {
+            res.status(500).send({
+                message: err
+            })
+        } else {
+            console.log('arquivo atualizado')
+
+            //retornando o arquivo criado em caso de status 200
+            //procura no pets.json, bate o id e printa na tela
+            const petFound = pets.find(pet => pet.id == id)
+            res.status(200).send(petFound)
+        }
+    })
+}
+
+
+const updateNome = (req, res) => {
+    let idPetRequest = req.params.id
+    let nomeFantasiaRequest = req.body.nomeFantasia
+    
+    //Procure em pets.json o ID e compare se está igual ao IdRequest e guarde na const petFound 
+    const petFound = pets.find (pet => pet.id == idPetRequest) 
+
+    //identificando o indice do pet no meu array através do PetFound (PetFound é o meu ID)
+    const petIndex = pets.indexOf (petFound) 
+
+    //se for encontrado o indice ele entra como nomeFantasiaRequest 
+    
+    if (petIndex >= 0) {
+        petFound.nomeFantasia = nomeFantasiaRequest
+
+    //Apaga a informação anterior e substitui pelo novo nome incluido na request
+    //ordem: encontra o indice de onde alterar, quantas infos serão alteradas, o que entra substituindo
+        pets.splice (petIndex, 1, petFound)
+
+        // vai escrever no /pets.json no formato de String em Json
+        fs.writeFile('.src/models/pets.json', JSON.stringfy(pets), 'utf8', function (err) {
+
+            //intaxe padrão do fs, incluindo tratamento de erro 
             if (err) {
                 res.status(500).send({
                     message: err
                 })
             } else {
-                console.log('arquivo atualizado')
+                console.log ("Nome Fantasia alterado");
+                //printa o pet que foi atualizado, bate o ID do pets.json com o IdRequest
+                const petUpdated = pets.find (pet => pet.id == idPetRequest)
+                res.status(200).send(petUpdated)
 
-                //retornando o arquivo criado em caso de status 200
-                //procura no pets.json, bate o id e printa na tela
-                const petFound = pets.find(pet => pet.id == id)
-            res.status(200).send(petFound)
-        }
-})
+            }})
+    } else {
+        res.status(404).send({ message: "Petshop não encontrado"})
+    }
 }
 
+
+
 module.exports = {
-    postPets
+    postPets,
+    updateNome
 }
