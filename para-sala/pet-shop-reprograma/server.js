3aea96783492d61d81d878a0d1715da86d726662
MajoBueno <buenomariajoao@gmail.com>
2022-06-04 13:19:19 -0300

    servidor configurado

---
+++ para-sala/pet-shop-reprograma/server.js
@@ -0,0 +1,14 @@
+// Invocando a biblio Express
+const express = require('express')
+// variável pra guardar a porta
+const PORT = 1313
+
+// servidor, deixa a gira girar
+app.listen(PORT, () => 
+{
+    console.log(`
+    =================================
+           ${PORT} rodando!
+    =================================
+    `)
+})
