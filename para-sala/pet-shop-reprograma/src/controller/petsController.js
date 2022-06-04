56c36887cec748247849102f10c23561183e75d0
Sara <saragiannini593@gmail.com>
2022-06-04 16:50:32 -0300

    correção de formatação

--- para-sala/pet-shop-reprograma/src/controller/petsController.js
+++ para-sala/pet-shop-reprograma/src/controller/petsController.js
@@ -6,13 +6,12 @@ const { response } = require('../app')
 const postPet = (request, response) => {
     const { id, nomeFantasia, endereco, telefone, atende } = request.body
     pets.push({ id: pets.length + 1, nomeFantasia, endereco, telefone, atende })
-
-    fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) { // gravando novo pet no array de pets
+    fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) {
         if (err) {
             response.status(500).send({ message: err })
         } else {
             console.log("Arquivo atualizado com sucesso!")
-            const petFound = pets.find(pet => pet.id == id) // recupero o pet que foi criado no array de pets 
+            const petFound = pets.find(pet => pet.id == id)
             response.status(200).send(petFound)
         }
     })
