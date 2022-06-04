b335a434eb3f550febc2543e703e1f399652c934
Sara <saragiannini593@gmail.com>
2022-06-04 14:28:44 -0300

    configura rota POST petshop

---
+++ para-sala/pet-shop-reprograma/src/router/petsRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/petsController')
+
+router.post('/adicionar', controller.postPet)
+
+module.exports = router
