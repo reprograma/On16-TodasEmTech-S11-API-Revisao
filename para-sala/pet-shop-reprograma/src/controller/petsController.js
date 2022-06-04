f229a71239241ad7412a4021ea1b36da69ae8ab9
Maysa <maysafig@gmail.com>
2022-06-04 15:11:39 -0300

    Corrigindo rota do post

--- para-sala/pet-shop-reprograma/src/controller/petsController.js
+++ para-sala/pet-shop-reprograma/src/controller/petsController.js
@@ -3,19 +3,18 @@ const fs = require('fs')
 
 const postPet = (req, res) => {
 
-    const {id, nomeFantasia, endereco, telefone, atende} = req.body
-    pets.push({id: (pets.length =1), nomeFantasia, endereco, telefone, atende})
+    const { id, nomeFantasia, endereco, telefone, atende } = req.body
+  pets.push({ id: pets.length + 1, nomeFantasia, endereco, telefone, atende })
 
-    fs.writeFile('./src/models/pets.json'), JSON.stringify(pets), 'utf8', function(err) {
-
-        if(err) {
-            res.status(500).send({message : err})
-        }else{
-            console.log('arquivo atualizado')
-            const petFound = pets.find(pet => pet.id == id)
-            res.status(200).send(petFound)
-        }
+  fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) { // gravando novo pet no array de pets
+    if (err) {
+        res.status(500).send({ message: err })
+    } else {
+        console.log("Arquivo atualizado com sucesso!")
+        const petFound = pets.find(pet => pet.id == id) // recupero o pet que foi criado no array de pets      
+        res.status(200).send(petFound)
     }
+})
 }
 
 
