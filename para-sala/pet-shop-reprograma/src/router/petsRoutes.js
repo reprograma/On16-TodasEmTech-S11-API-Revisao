ea3c03e9a9fd3a6c2fa1eeca8322b5f33f986cf7
Lia Laurindo <liamaralaurindo@gmail.com>
2022-06-04 14:27:19 -0300

    centralizando rotas

---
+++ para-sala/pet-shop-reprograma/src/router/petsRoutes.js
@@ -0,0 +1,18 @@
+//importando o express para usar o .router (que é uma função do Express)
+const express = require ('express')
+
+// guardando a função express.Router em uma const para simplificar a criação das rotas
+const router = express.Router()
+
+const controller = require ('../controller/petsController')
+
+//criando as rotas
+
+router.post ('/adicionar', controller.postPet)
+
+
+
+
+
+
+module.exports = router;
