205624090c5b33c34cdb4d5528c2813a99fa624f
Sara <saragiannini593@gmail.com>
2022-06-04 14:44:04 -0300

    adiciona mensagem status 200 postPet

--- para-sala/pet-shop-reprograma/src/controller/petsController.js
+++ para-sala/pet-shop-reprograma/src/controller/petsController.js
@@ -14,6 +14,7 @@ const petFound = pets.find(pet => pet.id == id) // recupero o pet que foi criado
 response.status(200).send(petFound)
 }
 })
+response.status(200).send({'message': 'Adicionado com sucesso'})
 }
 
 module.exports = {
