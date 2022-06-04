b7398702a586e655c85e3fae51666522b1d2c25d
Maysa <maysafig@gmail.com>
2022-06-04 16:56:37 -0300

    Criação da rota de patch

--- para-sala/pet-shop-reprograma/src/controller/petsController.js
+++ para-sala/pet-shop-reprograma/src/controller/petsController.js
@@ -4,49 +4,45 @@ const fs = require('fs')
 const postPet = (req, res) => {
 
     const { id, nomeFantasia, endereco, telefone, atende } = req.body
-  pets.push({ id: pets.length + 1, nomeFantasia, endereco, telefone, atende })
+    pets.push({ id: pets.length + 1, nomeFantasia, endereco, telefone, atende })
 
-  fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) { // gravando novo pet no array de pets
-    if (err) {
-        res.status(500).send({ message: err })
-    } else {
-        console.log("Arquivo atualizado com sucesso!")
-        const petFound = pets.find(pet => pet.id == id) // recupero o pet que foi criado no array de pets      
-        res.status(200).send(petFound)
-    }
-})
+    fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) { // gravando novo pet no array de pets
+        if (err) {
+            res.status(500).send({ message: err })
+        } else {
+            console.log("Arquivo atualizado com sucesso!")
+            const petFound = pets.find(pet => pet.id == id) // recupero o pet que foi criado no array de pets      
+            res.status(200).send(petFound)
+        }
+    })
 }
 
 const updateName = (req, res) => {
 
-    let idRequest = req.params.id
+    let idPetRequest = req.params.id
     let nomeFantasiaRequest = req.body.nomeFantasia
     
-    const petFound = pets.find(pet => pet.id == idRequest)
-    const petIndex = pets.indexOf(petFound)
-
-    if(petIndex > 0 ){
+    const petFound = pets.find(pet => pet.id == idPetRequest) // encontrando o pet
+    const petIndex = pets.indexOf(petFound) // identificando o indice do pet no meu array
+    
+    if (petIndex >= 0){
         petFound.nomeFantasia = nomeFantasiaRequest
+        //encontrou //remove //adiciona
         pets.splice(petIndex, 1, petFound)
 
         fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) { // gravando novo pet no array de pets
-            if (err) {
-                res.status(500).send({ message: err })
-            } else {
-                console.log('Seu arquivo foi alterado')
-                const petUpdated = pets.find(pet => pet.id == idRequest)
-                res.status(200).send(petUpdated)
-            }})       
-    }else{
-        res.status(400).send({ message: "Não encontramos esse petshop, verificar cadastro"})
+        if (err) {
+        res.status(500).send({ message: err })
+
+        } else {
+        console.log("Teu arquivo foi alterado");
+        const petUpdated = pets.find(pet => pet.id == idPetRequest)
+        res.status(200).send(petUpdated)
+        }})
+    } else {
+    res.status(404).send({ message: "Nao encontramos esse petshop, cadastra ai miga"})
     }
-
-}
-
-
-
-
-
+} 
 
 
 module.exports = {
