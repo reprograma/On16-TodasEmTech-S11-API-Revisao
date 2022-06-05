0e0f3da2a5c3278db679b79384654fb66e2446c5
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-06-05 15:53:16 -0300

    Criando rotas

--- para-sala/pet-shop-reprograma/src/router/petsRoutes.js
+++ para-sala/pet-shop-reprograma/src/router/petsRoutes.js
@@ -8,6 +8,7 @@ const controller = require('../controller/petsController')
 
 // Rotas:
 router.post('/adicionar', controller.postPet)
+router.patch('/name/:id',controller.updateName)
 
 
 module.exports = router
