4cf3a0860d3f77dbc9c3e05ce012d3676916fa19
Bárbara Baron <bcostabaron@gmail.com>
2022-06-06 21:19:22 -0300

    add getGamesById route

--- para-casa/games/src/router/gamesRoutes.js
+++ para-casa/games/src/router/gamesRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 const controller = require('../controller/gamesController')
 
 router.get('/all', controller.getAllGames)
+router.get('/:id', controller.getGamesById)
 
 module.exports = router
