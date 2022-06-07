b3c981957d5bc682d207f9022400d0bb2640ef90
Louise <louicosta.silva@gmail.com>
2022-06-07 16:17:41 -0300

    Modifiquei aquivo gamesRoutes.js

--- para-casa/games/src/routes/gamesRoutes.js
+++ para-casa/games/src/routes/gamesRoutes.js
@@ -1,1 +1,19 @@
-const games = require('../models/games.json')
+const express = require ('express')
+
+const router = express.Router()
+
+const controller = require('../controller/gamesController')
+
+router.get('/games', controller.getGames)
+router.get('/games/:id', controller.getIdGame)
+
+
+
+
+
+
+
+
+
+
+module.exports = router
