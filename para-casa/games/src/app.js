dc9d66bef646f15347d8761211c45e1fead00992
naiane-negri <naiane.negri@tagchat.com.br>
2022-06-07 20:38:07 -0300

    tarefa semana 11

---
+++ para-casa/games/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+const gameRouter = require('./router/gamesRouter')
+
+app.use('/games', gameRouter);
+
+module.exports = app
