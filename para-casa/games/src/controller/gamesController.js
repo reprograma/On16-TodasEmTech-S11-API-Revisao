6f9841fd4cec5b148f85994352e4b0f66907ae74
Bárbara Baron <bcostabaron@gmail.com>
2022-06-06 21:18:52 -0300

    add getGamesById controller

--- para-casa/games/src/controller/gamesController.js
+++ para-casa/games/src/controller/gamesController.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const games = require('../models/games.json')
 
@@ -10,11 +11,33 @@ const getAllGames = (req, res) => {
             }
         ])
     } catch(err) {
-        res.status(500).send({ message: 'Erro no server.'})
+        res.status(500).send({ message: 'Internal error'})
     }
 }
 
+
+const getGamesById = (req, res) => {
+
+    try {
+        let idRequest = req.params.id
+        let idFilter = games.filter(game => game.id == idRequest)
+
+        if (idFilter.length > 0) {
+            res.status(200).send(idFilter)
+        } else {
+            res.status(404).send({ message: 'Game not found'})
+        }
+    } catch(err) {
+        response.status(500).send({ message: 'Internal error'})
+    }
+
+}
+
+
+
+
 module.exports = {
-    getAllGames
+    getAllGames,
+    getGamesById
 }
 
