db9af61ad1a475a93083839fba9a92be3ee4f759
Bárbara Baron <bcostabaron@gmail.com>
2022-06-06 21:51:59 -0300

    add updateGameById route

--- para-casa/games/src/router/gamesRoutes.js
+++ para-casa/games/src/router/gamesRoutes.js
@@ -7,5 +7,6 @@ const controller = require('../controller/gamesController')
 router.get('/all', controller.getAllGames)
 router.get('/:id', controller.getGamesById)
 router.post('/register', controller.postNewGame)
+router.put('/update/:id', controller.updateGameById)
 
 module.exports = router
