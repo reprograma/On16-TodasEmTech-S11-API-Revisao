444ea969f138cd0806ae34bba696341225b899d1
Louise <louicosta.silva@gmail.com>
2022-06-08 20:51:00 -0300

    Adicionado alterações de códigos e rotas

--- para-casa/games/src/controller/gamesController.js
+++ para-casa/games/src/controller/gamesController.js
@@ -1,1 +1,83 @@
 const games = require('../models/games.json')
+
+
+
+const getGames = (req, res) => {
+    try {
+        res.status(200).json([{ "Games": games }])
+    } catch (err) {
+        res.status(500).send({ message: "Erro no servidor"})
+    }
+}
+
+const getIdGame = (req, res) => {
+    const idRequest = req.params.id
+    const idFilter = games.filter(game => game.id == idRequest)
+
+    if(idFilter.length > 0) {res.status(200).send(idFilter)
+
+    } else {res.status(404).send([{ message: "Id não localizado"}])}
+}
+
+const addGame = (req, res) => {
+    const {id, title, launchYear, consoles, liked} = req.body
+    games.push({ id: (games.length +1), title, launchYear, consoles, liked})
+    
+
+    if(games.length > -1) {
+        const newGame = games.find(game => game.id == id)
+        res.status(201).send([{message: "Novo jogo adicionado", games}])
+
+    } else { res.status(500).send([{message: "Erro no servidor"}])
+
+    }
+}
+
+const changeGame = (req, res) => {
+    const idRequest = req.params.id 
+    let gameRequest = req.body
+    let gamesFilter = games.findIndex(game => game.id == idRequest)
+
+    games.splice(gamesFilter, 1, gameRequest)
+
+    if(gamesFilter > -1) {
+        res.status(200).json([{ message: "Jogo atualizado", games }])
+    } else { res.status(404).send({message: "A informação não pode ser modificada"})}
+}
+
+const deleteGame = (req, res) => {
+    const idRequest = req.params.id
+    const gameId = games.findIndex(game => game.id == idRequest)
+    let idDeleted = games.filter(game => game.id == idRequest)
+
+    games.splice(gameId, 1)
+
+    if (gameId > 0) {
+        res.status(200).json([{message: "Jogo deletado", idDeleted, games}])
+
+    } else { res.status(404).send({message: "Não foi possível deletar o jogo"})}
+}
+
+const updateGame = (req, res) => {
+    const idRequest = req.params.id
+    const likedGame = req.body.liked
+    likedFilter = games.find(game => game.id == idRequest)
+
+    if(likedGame) {
+        likedFilter.liked = likedGame
+        res.status(200).json([{ message: "A tag liked foi alterada", games}])
+
+    } else { res.status(404).json([{message: "A tag liked não foi alterada"}])}
+
+}
+
+
+
+module.exports = {
+    getGames,
+    getIdGame,
+    addGame,
+    changeGame,
+    deleteGame,
+    updateGame
+}
