216353a6770bd8ddb2d0bf7b1fbf154447b5f409
Louise <louicosta.silva@gmail.com>
2022-06-08 20:52:45 -0300

    Feito alteração no arquivo gamesRoutes

--- para-casa/games/src/routes/gamesRoutes.js
+++ para-casa/games/src/routes/gamesRoutes.js
@@ -6,12 +6,10 @@ const controller = require('../controller/gamesController')
 
 router.get('/games', controller.getGames)
 router.get('/games/:id', controller.getIdGame)
-
-
-
-
-
-
+router.post('/add', controller.addGame)
+router.put('/change/:id', controller.changeGame)
+router.delete('/delete/:id', controller.deleteGame)
+router.patch('/update/:id/liked', controller.updateGame)
 
 
 
