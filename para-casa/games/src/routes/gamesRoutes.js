1cb0348d4c222009c6cbe45e2d82f225536ee886
Carol Alves <prof.carolalves90@gmail.com>
2022-06-08 12:58:57 -0300

    :bricks: ci: rotas para encontrar jogo

--- para-casa/games/src/routes/gamesRoutes.js
+++ para-casa/games/src/routes/gamesRoutes.js
@@ -3,5 +3,6 @@ const  router = express.Router()
 const controller = require("../controller/gamesController")
 
 router.get("/lista", controller.gamesLista)
+router.get("/buscarjogo/:id", controller.buscaJogo)
 
 module.exports = router
