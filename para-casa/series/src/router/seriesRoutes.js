1330a09479405d21177d341e2c2ff7ccc3413fae
Bárbara Baron <bcostabaron@gmail.com>
2022-06-07 23:31:36 -0300

    add series get routes

--- para-casa/series/src/router/seriesRoutes.js
+++ para-casa/series/src/router/seriesRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router()
 const controller = require('../controller/seriesController')
 
 router.get('/all', controller.getAllSeries)
+router.get('/genre', controller.getByGenre)
+router.get('/:id', controller.getSeriesById)
 
 module.exports = router
