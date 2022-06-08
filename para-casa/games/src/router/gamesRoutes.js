825bb38ea552e44c474b09a22c42124f8f66ae6b
lialaurindo <liamaralaurindo@gmail.com>
2022-06-08 19:42:44 -0300

    cadastrando endpoints requeridos na atividade

---
+++ para-casa/games/src/router/gamesRoutes.js
@@ -0,0 +1,28 @@
+//importando o express para usar o .router (que é uma função do Express)
+const express = require('express')
+
+// guardando a função express.Router em uma const para simplificar a criação das rotas
+const router = express.Router()
+
+//acessando a pasta controller para integrar a lógica das rotas
+const controller = require('../controller/gamesController')
+
+//criando as rotas:
+
+//retorna todos os jogos da lista Json
+router.get('/games', controller.getGames)
+
+//retorna apenas um jogo da lista
+router.get('/games/:id', controller.getGamesId)
+
+//cadastrar novo jogo
+router.post('/games', controller.addGames)
+
+//deletar um jogo especifico 
+router.delete('/games/:id', controller.delGames)
+
+//atualizar avaliação do jogo, campo "liked" da lista Json
+router.patch('/games/:id/liked', controller.updateGames)
+
+
+module.exports = router;
