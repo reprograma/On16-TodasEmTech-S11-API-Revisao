f33d0b039d89e2186233ae5bb9fc8246dd9a3f44
Bárbara Baron <bcostabaron@gmail.com>
2022-06-07 23:31:14 -0300

    add series get controllers

--- para-casa/series/src/controller/seriesController.js
+++ para-casa/series/src/controller/seriesController.js
@@ -1,6 +1,5 @@
 const series = require('../models/series.json')
 
-
 const getAllSeries = (req, res) => {
 
     try {
@@ -11,6 +10,57 @@ const getAllSeries = (req, res) => {
     }
 }
 
+const getByGenre = (req, res) => {
+
+    try {
+
+        const genreRequest = req.query.genre.toLowerCase()
+
+        let genreFilter = series.filter(serie => serie.genre.toLowerCase().includes(genreRequest))
+
+        if (genreFilter.length > 0) {
+            
+            res.status(200).send(genreFilter)
+
+        } else {
+
+            res.status(404).send({message: 'Not found'})
+        }
+
+    } catch(err) {
+        res.status(500).send({ message: 'Internal error'})
+    }
+}
+
+const getSeriesById = (req, res) => {
+
+    try {
+
+        const idRequest = req.params.id
+
+        const idFilter = series.filter(series => series.id == idRequest)
+
+        if (idFilter.length > 0) {
+
+            res.status(200).send(idFilter)
+
+        } else {
+
+            res.status(404).send({ message: 'Not found'})
+        }
+ 
+    } catch(err) {
+
+        res.status(500).send({ message: 'Internal error'})
+    }
+
+
+
+}
+
+
 module.exports = {
-    getAllSeries
+    getAllSeries,
+    getByGenre,
+    getSeriesById
 }
