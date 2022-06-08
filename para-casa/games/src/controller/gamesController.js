8c8554d04754ba14bb7156ac214454f50f0d7468
Carol Alves <prof.carolalves90@gmail.com>
2022-06-08 13:34:46 -0300

    :sparkles: feat: codigo para cadastrar jogo

--- para-casa/games/src/controller/gamesController.js
+++ para-casa/games/src/controller/gamesController.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express")
 const gamesJson = require("../models/games.json")
 
 const gamesLista = (request, response) => {
@@ -25,7 +26,29 @@ const buscaJogo = (request, response) => {
     }
 }
 
+const cadastraJogo = (request, response) => {
+    try{
+        const {id, title, launchYear, consoles, liked} = request.body
+
+        let novoJogo = {
+            id: (gamesJson.length + 1), title, launchYear, consoles, liked
+        }
+
+    gamesJson.push(novoJogo)
+
+        response.status(201).json({
+            message: "Jogo cadastrado",
+            novoJogo
+        })
+    } catch (err) {
+        response.status(500).send({
+            message: "Erro ao cadastrar"
+        })
+    }
+}
+
 module.exports ={
     gamesLista,
-    buscaJogo
+    buscaJogo,
+    cadastraJogo
 }
