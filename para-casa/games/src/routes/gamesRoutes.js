fcbeeb079ab8ee22a5d03200e25b04ca4b5dc82c
Carol Alves <prof.carolalves90@gmail.com>
2022-06-08 16:20:51 -0300

    :bricks: ci: rota para dar like no jogo

--- para-casa/games/src/routes/gamesRoutes.js
+++ para-casa/games/src/routes/gamesRoutes.js
@@ -7,5 +7,6 @@ router.get("/buscarjogo/:id", controller.buscaJogo)
 router.post("/cadastrar", controller.cadastraJogo)
 router.put("/atualizar/:id", controller.atualizaJogo)
 router.delete("/deletar/:id", controller.deletaJogo)
+router.patch("/liked/:id", controller.likedGame)
 
 module.exports = router
