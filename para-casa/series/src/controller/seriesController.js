437493d786c1cfc40bdb49ac9a3731c7aba76cc8
MajoBueno <buenomariajoao@gmail.com>
2022-06-07 21:55:08 -0300

    adicionei função para buscar por ID, filtro por genre não funcionou

--- para-casa/series/src/controller/seriesController.js
+++ para-casa/series/src/controller/seriesController.js
@@ -16,13 +16,49 @@ const allSeries = (req, res) => {
 }
 
 }
-
+// buscar series por genero
 const byGenre = (req, res) => {
-    
+    // buscando genero para filtrar e deixando em caixa baixa
+    let genre = req.query.genre.toLowerCase()
+    // filtrando por genero
+    let serieFiltered = series.filter((serie) => {
+        // preciso passar pelo array para deixar em caixa baixa as strings do json
+    genreLowerCase = serie.genre.map((string) => string.toLowerCase())
+    // me de o retorno que inclua a requisição
+        return genreLowerCase.includes(genre)
+    })
+        
+// se ao filtrar encontrou algum
+  if (serieFiltered.length > 0) {
+      //me mostra
+    res.status(200).send(serieFiltered)
+// se não
+} else {
+    // me avisa
+    res.status(404).send({
+        message: "não encontrei series desse gênero"
+    })
 }
-
+}    
+// encontrar por id
 const findId =  (req, res) => {
-    
+    try {
+        let serieId = req.params.id
+
+        let sfiltered = series.filter((s) => s.id == serieId)
+
+    if (sfiltered.length > 0) {
+    res.status(200).json({
+        message: "Série encontrada!", sfiltered
+        })
+    } else { 
+    res.status(404).send({
+        message: "não encontrei esse id"
+        })
+    }
+} catch(err) {
+        res.status(500).send({ message: "Error" })
+    }
 }
 
 const newSerie =  (req, res) => {
