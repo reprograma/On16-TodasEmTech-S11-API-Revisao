e53af10bb2ad0d06b80b9e1cd6ecc3b6d5d83352
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-06-08 18:51:15 -0300

    Criação das rotas de get,put e delete

--- para-sala/pet-shop-reprograma/src/router/petsRoutes.js
+++ para-sala/pet-shop-reprograma/src/router/petsRoutes.js
@@ -9,6 +9,11 @@ const controller = require('../controller/petsController')
 // Rotas:
 router.post('/adicionar', controller.postPet)
 router.patch('/name/:id',controller.updateName)
-
+router.get('/listar', controller.getAllpets)
+router.get('/listar/:id', controller.getPetsId)
+router.get('/listar/atendimento', controller.getPetsAtende)
+router.get('/listar/endereco', controller.getPetsEndereco)
+router.put('/atualizar/:id', controller.updateAllDatas)
+router.delete('/deletar/:id', controller.deletePets)
 
 module.exports = router
