14d620ce0792eb02c4b57cdb0f79bd9eb5d3a324
Bárbara Baron <bcostabaron@gmail.com>
2022-06-09 00:47:56 -0300

    add deleteSeriesById route

--- para-casa/series/src/router/seriesRoutes.js
+++ para-casa/series/src/router/seriesRoutes.js
@@ -8,5 +8,6 @@ router.get('/all', controller.getAllSeries)
 router.get('/genre', controller.getByGenre)
 router.get('/:id', controller.getSeriesById)
 router.post('/newSerie', controller.postNewSerie)
+router.delete('/delete/:id', controller.deleteSerieById)
 
 module.exports = router
