7d6ebd253cfa6041f985443fc22ffe047717bf78
caroliinaasilva <carolinadesousa84@yahoo.com>
2022-06-10 09:45:48 -0300

    entrega atividade

--- para-casa/games/src/controller/controllerGames.js
+++ para-casa/games/src/controller/controllerGames.js
@@ -21,15 +21,15 @@ const getbyId = (request, response) => {
         }])
     }
 }
-const add = (request, response) =>{
-    try{
-        const {id, title, launchYear, consoles, liked} = request.body
+const add = (request, response) => {
+    try {
+        const { id, title, launchYear, consoles, liked } = request.body
 
         let novoJogo = {
             id: (gamesJson.length + 1), title, launchYear, consoles, liked
         }
 
-    gamesJson.push(novoJogo)
+        gamesJson.push(novoJogo)
 
         response.status(201).json({
             message: "Jogo cadastrado",
@@ -41,9 +41,69 @@ const add = (request, response) =>{
         })
     }
 }
+const update = (request, response) => {
+    try {
+        const idRequest = request.params.id
+        let favoriteRequest = request.body
+        let findgame = gamesJson.findIndex(game => game.id == idRequest)
+        gamesJson.splice(findgame, 1, favoriteRequest)
+
+        response.status(201).json({
+            message: "atualizado gatinha",
+            gamesJson
+        })
+    } catch (err) {
+        response.status(500).send({
+            message: "Erro ao atualizar"
+        })
+    }
+}
+
+const deleteGame = (request, response) => {
+    const idRequest = request.params.id
+    const indiceGame = gamesJson.findIndex(game => game.id == idRequest)
+
+    gamesJson.splice(indiceGame, 1)
+
+    if (indiceGame) {
+        response.status(200).json([
+            {
+                message: 'game deletado', gamesJson
+            }
+        ])
+    } else {
+        response.status(404).send([
+            {
+                message: 'erro ao deletar'
+            }
+        ])
+    }
+
+}
+const updateLike = (request, response) => {
+    const idRequest = request.params.id
+    const likeRequest = request.body.liked
+
+    findflike = gamesJson.find((game) => game.id == idRequest)
+    if (findflike) {
+        findflike.liked = likeRequest
+        response.status(200).json([{
+            message: 'like atualizado', gamesJson
+        }])
+
+    } else {
+        response.status(404).send([{
+            message: 'erro ao atualizar'
+        }])
+    }
+}
+
 module.exports = {
     getAll,
     getbyId,
-    add
+    add,
+    update,
+    deleteGame,
+    updateLike
 
 }
--- para-casa/games/src/routes/gamesRoutes.js
+++ para-casa/games/src/routes/gamesRoutes.js
@@ -7,6 +7,8 @@ const controller = require('../controller/controllerGames')
 router.get('/listar', controller.getAll)
 router.get('/bygame/:id', controller.getbyId)
 router.post('/add', controller.add)
-router.put('/update', controller.update)
+router.put('/update/:id', controller.update)
+router.delete('/deletar/:id', controller.deleteGame)
+router.patch('/updatelike/:id', controller.updateLike)
 
 module.exports = router
