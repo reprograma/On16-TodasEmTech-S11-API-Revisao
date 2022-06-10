c62976a8538fd483dfd04e6533259715d4cf0511
Carol Alves <prof.carolalves90@gmail.com>
2022-06-10 12:13:28 -0300

    :bricks: rota para buscar serie

--- para-casa/series/src/routes/seriesRoutes.js
+++ para-casa/series/src/routes/seriesRoutes.js
@@ -4,6 +4,8 @@ const controller = require("../controller/seriesController")
 
 router.get("/lista", controller.getSeries)
 router.get("/genero", controller.getGenero)
+router.get("/buscaserie/:id", controller.buscaSerie)
+
 
 //desafio
 router.post("/:id/season/:seasonId/episode", controller.addEpisode)
