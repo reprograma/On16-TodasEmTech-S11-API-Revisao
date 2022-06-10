fa28c2fe4b65c9fc491d9f91888567019a1303a9
Maysafig <maysafig@gmail.com>
2022-06-10 00:44:13 -0300

    Criação da rota de delete

--- para-sala/pet-shop-reprograma/src/controller/petsController.js
+++ para-sala/pet-shop-reprograma/src/controller/petsController.js
@@ -1,5 +1,6 @@
 const pets = require('../models/pets.json')
 const fs = require('fs')
+const { response } = require('../app')
 
 const postPet = (req, res) => {
 
@@ -40,12 +41,118 @@ const updateName = (req, res) => {
         res.status(200).send(petUpdated)
         }})
     } else {
-    res.status(404).send({ message: "Nao encontramos esse petshop, cadastra ai miga"})
+    res.status(404).send({ message: "PetShop não encontrado"})
     }
 } 
+
+const allPets = (req, res) => {
+
+    try{
+        res.status(200).json([{ Petshops : pets }])
+    }catch{
+        res.status(500).send({ message : "Erro no server" })
+    }
+}
+
+const petsById = (req, res) => {
+
+    const idRequest = req.params.id
+
+    const idFilter = pets.filter( petshop => petshop.id == idRequest )
+
+    if( idFilter.length > 0 ){
+        res.status(200).send(idFilter)
+    }else{
+        res.status(404).send({ message : "Petshop não encontrado"})
+    }
+
+}
+
+const petsByState = (req, res) => {
+
+    let stateRequest = req.query.endereco
+
+    let stateFilter = pets.filter( pet => pet.endereco.includes(stateRequest))
+
+    if (stateFilter.length > 0 ){
+        res.status(200).send(stateFilter)
+    }else{
+        res.status(400).send({messege : "Estado não localizado"})
+    }
+}
+
+const petsService = (req, res) => {
+
+    let serviceRequest = req.query.atende
+
+    let serviceFilter = pets.filter( pets => pets.atende.includes(serviceRequest))
+
+    if( serviceFilter.length > 0){
+        res.status(200).send(serviceFilter)
+    }else{
+        res.status(400).send({ messege : "Atendimento não encontrado"})
+    }
+}
+
+
+const petUpdate = (req, res) => {
+
+    const idRequest = req.params.id
+    const { nomeFantasia, endereco, telefone, atende } = req.body
+
+
+    const petFound = pets.find(pet => pet.id == idRequest) // encontrando o pet
+    const petIndex = pets.indexOf(petFound) // identificando o indice do pet no meu array
+    
+  
+    if (petIndex != -1 ){
+
+        pets[petIndex] = {id: parseInt(idRequest), nomeFantasia, endereco, telefone, atende}
+        res.status(200).send(pets[petIndex])
+
+        fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) { // gravando novo pet no array de pets
+        if (err) {
+        res.status(500).send({ message: err })
+
+        } else {
+        console.log("As informações foram alteradas");
+        const petUpdated = pets.find(pet => pet.id == idRequest)
+        res.status(200).send(petUpdated)
+        }})
+    } else {
+    res.status(404).send({ message: "Petshop não encontrado"})
+    }
+   
+} 
+
+const petDelete = (req, res) => {
+
+    const idRequest = req.params.id
+    const indexPet = pets.findIndex( pet => pet.id == idRequest)
+
+    if(indexPet != -1){
+        pets.splice(indexPet, 1)
+
+        fs.writeFile("./src/models/pets.json", JSON.stringify(pets), 'utf8', function (err) { // gravando novo pet no array de pets
+        if (err) {
+        res.status(500).send({ message: err })
+
+        } else {
+            res.status(200).send({message : "O petshop foi deletado"})
+        }})
+    }else{
+        res.status(404).json({ message : "Petshop não encontrado"})
+    }
+}
 
 
 module.exports = {
     postPet,
-    updateName
+    updateName,
+    allPets,
+    petsById,
+    petsByState,
+    petsService,
+    petUpdate,
+    petDelete
 }
--- para-sala/pet-shop-reprograma/src/router/petsRoutes.js
+++ para-sala/pet-shop-reprograma/src/router/petsRoutes.js
@@ -5,6 +5,12 @@ const controller = require('../controller/petsController')
 
 router.post('/adicionar', controller.postPet)
 router.patch('/name/:id', controller.updateName)
+router.get('/allpets', controller.allPets)
+router.get('/find/:id', controller.petsById)
+router.get('/bystate',controller.petsByState)
+router.get('/services', controller.petsService)
+router.put('/update/:id', controller.petUpdate)
+router.delete('/delete/:id', controller.petDelete)
 
 
 module.exports = router
