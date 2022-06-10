158fe950f52de8aead6fd422265ec34a87e034bb
Maysafig <maysafig@gmail.com>
2022-06-10 18:30:42 -0300

    Correção do nome das pastas da API de séries

---
+++ para-casa/series/src/routes/seriesRoutes.js
@@ -0,0 +1,10 @@
+const controller = require("../controller/seriesController")
+const express = require("express")
+const router = express.Router()
+
+router.get("/all", controller.seriesAll)
+router.get("/genre", controller.seriesGenre)
+router.get("/:id",controller.seriesById)
+router.post("/add", controller.serieAdd)
+router.patch("/liked/:id", controller.serieLiked)
+router.delete("/:id", controller.serieDelete)
