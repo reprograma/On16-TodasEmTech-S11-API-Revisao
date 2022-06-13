ec2ce46fbf066a5f08a79645c7c08da858e906f1
Débora Christyna <cdias.proj@gmail.com>
2022-06-13 05:58:23 -0300

    add função delete - ajuste função attGames com uso fs

--- para-casa/games/src/app.js
+++ para-casa/games/src/app.js
@@ -3,6 +3,6 @@ const app = express()
 app.use(express.json())
 const gamesRotas = require('./router/gamesRoutes')
 //Rota raiz
-app.use('./games', gamesRotas)
+app.use('/games', gamesRotas)
 
 module.exports = app
