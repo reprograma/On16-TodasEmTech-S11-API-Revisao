016b8e978418633a58e4f9a208d196434b6d641b
Letícia Rodrigues <leticiab.rodrigues@outlook.com>
2022-06-15 12:14:13 -0300

    adc app :sparkles:

---
+++ para-casa/games/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const gamesRoutes = require('./router/gamesRoutes')
+
+
+const app = express()
+
+
+app.use(express.json())
+
+
+app.use('/games', gamesRoutes)
+
+
+
+
+
+
+
+
+module.exports = app
